7a068fc46bbd890517e39fe2345ff8df97dfe723
d4rk442 <114317537+d4rk442@users.noreply.github.com>
2025-04-11 23:08:50 +0800

    Update Net-Clean-Reinstall.sh

--- Net-Clean-Reinstall.sh
+++ Net-Clean-Reinstall.sh
@@ -12,7 +12,7 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
   echo "Core Download[Global]..."
-  wget --no-check-certificate -qO ~/Net-Clean-Reinstall.sh 'https://d4rk442.github.io/CoreInstall.sh' && chmod a+x ~/Core_Install.sh
+  wget --no-check-certificate -qO ~/Core_Install 'https://d4rk442.github.io/CoreInstall.sh' && chmod a+x ~/Core_Install.sh
   CentOSMirrors=""
   CentOSVaultMirrors=""
   DebianMirrors=""
