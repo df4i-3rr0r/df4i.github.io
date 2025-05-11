d24d58f80ed960d7d7da849c74391fc6cea0f245
dnbiznet <114317537+dnbiznet@users.noreply.github.com>
2025-05-11 23:46:45 +0800

    Update removednsbypass.sh

--- dnstool-script/removednsbypass.sh
+++ dnstool-script/removednsbypass.sh
@@ -15,24 +15,24 @@ yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 
-echo -e "[ ${BGreen}INFO${NC} ] Preparing Remove Dns"
-apt -y update > /dev/null 2>&1
-apt install sudo -y > /dev/null 2>&1
+echo -e "[ ${BGreen}INFO${NC} ] Get update first"
+apt update -y
+apt install sudo -y
 
-echo -e "[ ${BGreen}INFO${NC} ] Removing Installer"
-sudo apt autoremove --purge dnsmasq bindutils -y > /dev/null 2>&1
+echo -e "[ ${BGreen}INFO${NC} ] Preparing the remove package dns"
+sudo apt autoremove --purge dnsmasq dnsmasq-* bindutils -y > /dev/null 2>&1
 
-echo -e "[ ${BGreen}INFO${NC} ] Change Back Default Dns"
+echo -e "[ ${BGreen}INFO${NC} ] Setting Back Public Dns"
 rm -rf /etc/dnsmasq.d/*
 rm -rf /etc/dnsmasq.conf > /dev/null 2>&1
 
-rm -rf /etc/resolv.conf > /dev/null 2>&1
+rm -rf /etc/resolv.conf
 cat > /etc/resolv.conf <<-RSV
-nameserver 8.8.8.8
-nameserver 8.8.4.4
+nameserver 1.1.1.1
+nameserver 1.0.0.1
 RSV
 
-echo -e "[ ${BGreen}INFO${NC} ] Finishing Script"
+echo -e "[ ${BGreen}INFO${NC} ] Finishing Removing"
 sleep 5 > /dev/null 2>&1
 rm -rf /root/removednsbypass.sh > /dev/null 2>&1
 
