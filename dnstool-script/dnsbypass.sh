1527e7db8edae91d0b769adb98fdc85ebe1eb16d
dnbiznet <114317537+dnbiznet@users.noreply.github.com>
2025-05-11 23:55:08 +0800

    Update dnsbypass.sh

--- dnstool-script/dnsbypass.sh
+++ dnstool-script/dnsbypass.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 #Script By Dyno
 
-red='\e[1;31m'
-green='\e[0;32m'
-yell='\e[1;33m'
-tyblue='\e[1;36m'
-BRed='\e[1;31m'
-BGreen='\e[1;32m'
-BYellow='\e[1;33m'
-BBlue='\e[1;34m'
-NC='\e[0m'
-purple() { echo -e "\\033[35;1m${*}\\033[0m"; }
-tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
-yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
-green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-
 systemctl disable systemd-resolved
 
 rm -rf /etc/resolv.conf
@@ -24,14 +9,15 @@ nameserver 1.1.1.1
 nameserver 1.0.0.1
 RSV
 
-echo -e "[ ${BGreen}INFO${NC} ] Get update first"
-apt update -y
-apt install sudo -y
+echo -e "Get update first"
+apt-get update -y
 
-echo -e "[ ${BGreen}INFO${NC} ] Preparing the install file"
-sudo apt install dnsmasq bindutils -y
+apt-get install sudo -y
 
-echo -e "[ ${BGreen}INFO${NC} ] Creating dns bypass"
+echo -e "Preparing the install file"
+sudo apt-get install dnsmasq bindutils -y
+
+echo -e "Creating dns bypass"
 rm -rf /etc/dnsmasq.d/*
 rm -rf /etc/dnsmasq.conf
 cat > /etc/dnsmasq.conf <<-DNS1
@@ -74,11 +60,11 @@ cat > /etc/resolv.conf <<-RSV
 nameserver 127.0.0.1
 RSV
 
-echo -e "[ ${BGreen}INFO${NC} ] Finishing Installer"
+echo -e "Finishing Installer"
 sleep 5
 
 rm -rf /root/dnsbypass.sh
 
-echo -e "[ ${BGreen}INFO${NC} ] Your Server Will Reboot Now"
+echo -e "Your Server Will Reboot Now"
 reboot
 
