4f2bfd82f825d8a691753eb27dcdf2d395bddedc
dnbiznet <114317537+dnbiznet@users.noreply.github.com>
2025-06-04 20:19:35 +0800

    Update tw1.sh

--- recovery-script/tw1.sh
+++ recovery-script/tw1.sh
@@ -1,5 +1,5 @@
 #Bash
-#DynoMinimTweak
+#DynoLite Tweak
 
 rm -rf /etc/resolv.conf
 cat > /etc/resolv.conf <<-DS0
@@ -7,8 +7,12 @@ nameserver 1.1.1.1
 nameserver 1.0.0.1
 DS0
 
+opkg update
+
+rm -rf /tmp/resolv.conf*
+rm -rf /tmp/resolv.conf.d
+rm -rf /etc/resolv.conf.d
 mkdir /etc/resolv.conf.d
-rm -rf /etc/resolv.conf.d/cf.conf
 rm -f /etc/resolv.conf.d/cf.conf
 cat > /etc/resolv.conf.d/cf.conf <<-DS1
 nameserver 1.1.1.1
@@ -20,14 +24,19 @@ rm -rf /etc/sysctl.d/12-tcp-bbr.conf
 cat > /etc/sysctl.d/99-bbr.conf <<-DS5
 net.core.default_qdisc=fq
 net.ipv4.tcp_congestion_control=bbr
+net.ipv6.conf.all.use_tempaddr=0
+net.ipv6.conf.default.use_tempaddr=0
 DS5
 sysctl -p
 
-opkg remove --autoremove *ddns* *wireguard* *openvpn* *sqm* *turboacc* --force-depends
+opkg update
 
-rm -rf /etc/ddns/*
+opkg remove *ddns* *wireguard* *openvpn* *chinadns* *sqm* *turboacc* --force-depends
 
-opkg install luci-app-ddns
+rm -rf /etc/init.d/ipsec
+rm -rf /etc/config/ddns
+
+opkg install luci-app-ddns openssh-sftp-server nano htop curl wget
 
 cd /tmp
 wget https://dnbiznet.github.io/recovery-script/luci-theme-neobird_1.99-202201272020_all.ipk
@@ -35,22 +44,15 @@ opkg install luci-theme-neobird_1.99-202201272020_all.ipk
 rm -rf luci-theme-neobird_1.99-202201272020_all.ipk
 cd
 
-uci set dhcp.@dnsmasq[0].noresolv='0'
-uci set dhcp.@dnsmasq[0].domainneeded='0'
-uci set dhcp.@dnsmasq[0].nonwildcard='0'
-uci set dhcp.@dnsmasq[0].boguspriv='0'
-uci set dhcp.@dnsmasq[0].expandhosts='0'
-uci delete dhcp.@dnsmasq[0].local
-uci delete dhcp.@dnsmasq[0].domain
+wget -q https://raw.githubusercontent.com/dvh-patcher/system/refs/heads/main/speed.sh
+chmod +x speed.sh
+./speed.sh
+
 uci set dhcp.@dnsmasq[0].resolvfile='/etc/resolv.conf.d/cf.conf'
 uci commit dhcp
-
-uci set network.wwan0.peerdns='0'
-uci delete network.wwan0.dns
-uci commit network
-uci commit
 
 uci set qmodem_ttl.main.enable='1'
 uci commit qmodem_ttl
 
+rm -rf /root/*
 reboot
