6eb40e6671608c3317b2a25837c058f9e7ad0372
DF4I <114317537+df4i-3rr0r@users.noreply.github.com>
2025-06-28 02:25:43 +0800

    Update cert.sh

--- vpstool-script/multi-tool/cert.sh
+++ vpstool-script/multi-tool/cert.sh
@@ -6,11 +6,11 @@ rm -rf /usr/local/etc/xray/xray.key
 
 cd /usr/local/etc/xray/
 
-wget https://df4i.github.io/vpstool-script/multi-tool/xray.crt
+wget https://github.com/df4i-3rr0r/df4i.github.io/blob/main/vpstool-script/multi-tool/xray.crt
 
 chmod 755 /usr/local/etc/xray/xray.crt
 
-wget https://df4i.github.io/vpstool-script/multi-tool/xray.key
+wget https://github.com/df4i-3rr0r/df4i.github.io/blob/main/vpstool-script/multi-tool/xray.key
 
 chmod 755 /usr/local/etc/xray/xray.key
 
